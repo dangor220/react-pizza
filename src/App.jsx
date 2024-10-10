5abdd5da37f730e3d07988ac214662fb71f221fd
Danil Gordeev <dan.gor220@yandex.ru>
2024-10-10 23:52:40 +0300

    feat: divided the html markup into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import './App.css';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import './scss/app.scss';
 
-function App() {
-	return <></>;
+export default function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title={'Мексиканская'} price={350} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default App;
